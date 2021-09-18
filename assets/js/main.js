29124a7818774b6053a1a6c68281727a27378194
Arp-G <arpanghoshal3@gmail.com>
2021-09-18 17:38:13 +0530

    working example

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,12 @@ window.addEventListener('load', () => {
   // Helper function to generate a unique call id
   const uid = () => Date.now().toString(36) + Math.random().toString(36).substr(2);
 
-  // Helper function to await joining the channel
+  // Helper function to await joining the channel, resturs response from server on channel join success
   const join_channel = (channel, call_id) => new Promise((resolve, reject) => {
     channel.join()
       .receive("ok", resp => {
         console.log(`Joined topic call:${call_id} successfully`, resp);
-        resolve(resp);
+        resolve(resp); // Resolve with on join response from server
       })
       .receive("error", resp => {
         console.log("Unable to join topic call:${call_id}", resp);
@@ -30,7 +30,7 @@ window.addEventListener('load', () => {
     the webRTC connection handles all the complexity related to the peer-2-peer networking and media streaming under the hood.
   */
 
-  // Free STUN servers avaibale from google
+  // Free STUN servers available from google
   const servers = {
     iceServers: [
       {
@@ -91,19 +91,22 @@ window.addEventListener('load', () => {
     channel = socket.channel(`call:${callId}`, { type: "caller" });
     await join_channel(channel, callId);
 
-    callInput.value = callId; // This is unique auto generated ID of a document from firestore, peer 2 will use this to answer to the offer made by peer 1
+    callInput.value = callId; // This is an unique ID, peer 2 will use this to answer to the offer made by peer 1
 
     /*
-      Get candidates for caller, save to db
+      Save caller candidates to db
   
-      Set up an event listener to save ICE candidates from yourself or your local peer
-      to firestore db collection "offerCandidates".
+      Sets up an event listener to send ICE candidates from peer-1
+      to the signaling server.
+
+      The signaling server will save these candidates and forward them to peer-2
+      when peer-2 connects with the server.
   
       An ICE candidate contains a potential IP address and port pair 
       that can be used to establish a peer-2-peer connection.
   
       Your ICE candidates are generated when `pc.setLocalDescription` is called
-      so this listener must be setup before calling pc.setLocalDescription
+      so this listener must be setup before calling pc.setLocalDescription.
     */
     pc.onicecandidate = (event) => {
       if (event.candidate) {
@@ -133,7 +136,7 @@ window.addEventListener('load', () => {
     // Save the offer to firestore db collection "offerCandidates", 
     // the offer ice candidates are saved via the "onicecandidate" event listener
     channel.push("offer", { offer }, 10000);
-
+    console.log("Pushed offer from peer-1", offer);
     // ---------------------------------------------------
 
     // Update local RTCPeerConnection with answer from remote peer
@@ -143,16 +146,14 @@ window.addEventListener('load', () => {
       with the answer. 
     */
     channel.on("answer", ({ answer }) => {
-      console.log("Received Answer", answer);
-      const candidate = new RTCIceCandidate(answer);
-      pc.addIceCandidate(candidate);
+      console.log("Received Answer from peer-2", answer);
+      const answerDescription = new RTCSessionDescription(answer);
+      pc.setRemoteDescription(answerDescription);
     });
 
     /*
-      We also need to listen for updates to the ICE candidates from the remote peer connection.
-      This data is stored in firestore "answerCandidates" doc and we listen for changes on it.
-  
-      When answered, add remote peers ICE candidate to the local RTCPeerConnection.
+      Here we setup a listener on the signaling server channel to listen for ice candidates from peer-2.
+      Once peer-2 ice candidates are available we add them to the RTCPeerConnection object
     */
     channel.on("ice_candidate", ({ ice_candidate }) => {
       console.log("Received ICE candidate for peer 2", answer);
@@ -166,20 +167,16 @@ window.addEventListener('load', () => {
   // -----------------------------------------------------------------------
 
 
-  // 3. Answer the call with the unique ID, this happens on the remote peer
+  // 3. Answer the call with the unique ID, this happens on the remote peer/peer-2
   answerButton.onclick = async () => {
     const callId = callInput.value;
-
+    let caller_candidates;
     // TODO: Fix duplicate channel join
     if (!channel) {
       // Join channels with a topic:
       channel = socket.channel(`call:${callId}`, { type: "callee" });
-      const caller_candidates = await join_channel(channel, callId);
-
-      caller_candidates.forEach(candidate => {
-        const ice_candidate = new RTCIceCandidate(candidate);
-        pc.addIceCandidate(ice_candidate);
-      });
+      caller_candidates = await join_channel(channel, callId);
+      console.log(`Got ${caller_candidates.length} ice candidates from caller on channel join`, caller_candidates);
     }
 
     const offer = await new Promise((resolve, reject) => {
@@ -189,7 +186,7 @@ window.addEventListener('load', () => {
         .receive("timeout", () => reject("Networking issue..."));
     });
 
-    console.log("GOT Offer", offer);
+    console.log("Got offer from caller", offer);
 
     if (!offer) {
       channel.leave();
@@ -197,17 +194,9 @@ window.addEventListener('load', () => {
       return;
     }
 
-    /*
-      Get candidates for this remote peer, and save to db collection "answerCandidates"
-  
-      An ICE candidate contains a potential IP address and port pair 
-      that can be used to establish a peer-2-peer connection.
-  
-      Your ICE candidates are generated when `pc.setLocalDescription` is called
-      so this listener must be setup before calling pc.setLocalDescription
-    */
+    // Send callee/remote-peer ice candidates to signaling server, these are send to the caller by the signaling server
     pc.onicecandidate = (event) => {
-      if (event.candidate) channel.push("ice_candidate", { ice_candidate: event.candidat, type: "callee" }, 10000);
+      if (event.candidate) channel.push("ice_candidate", { ice_candidate: event.candidate, type: "callee" }, 10000);
     };
 
     // Get the offer made by the other peer and set it as the offer description.
@@ -217,25 +206,34 @@ window.addEventListener('load', () => {
     // Create a new answer for the offer and set it as the local description.
     // So for the caller peer we set the "offer" as the local description and for the remote peer we set the answer as the local description
     const answerDescription = await pc.createAnswer();
-    await pc.setLocalDescription(answerDescription); // This also triggers the "onicecandidate" event and saves the remote peers ICE candidates to the "answerCandidates" collection in firestore
 
-    // Create a JS object which contains the SDP answer data that can be saved to firestore db collection "answerCandidates"
+    // This also triggers the "onicecandidate" event and sends the remote peers ICE candidates to signaling server which further sends them to the caller
+    await pc.setLocalDescription(answerDescription);
+
+    // Create a JS object which contains the SDP answer data
     const answer = {
       type: answerDescription.type,
       sdp: answerDescription.sdp,
     };
 
-    // Save the answer to firestore db collection "answerCandidates", 
-    // the answer ice candidates are saved via the "onicecandidate" event listener
+    // Send the answer to the signaling server which further sends it to the caller
     channel.push("answer", { answer }, 10000);
+    console.log('Push answer from callee', answer);
+
+    // https://stackoverflow.com/questions/38198751/domexception-error-processing-ice-candidate
+    caller_candidates.forEach(candidate => {
+      console.log('Adding ice candidate', candidate);
+      const ice_candidate = new RTCIceCandidate(candidate);
+      pc.addIceCandidate(ice_candidate);
+    });
 
     /*
-      Setup an event listener to listen for changes in the "offerCandidates" collection.
-      So when ICE candidates for the caller/peer 1 are added they can be updated in remote/peer 2's
-      RTCPeerConnection object. We had similar thing for caller as well.
+      Setup an event listener to listen for new ice candidates from the caller.
+      So when ICE candidates for the callee are received they are added to the
+      RTCPeerConnection object for the callee. We had similar logic for the caller as well.
     */
     channel.on("new_ice_candidate", ({ ice_candidate }) => {
-      console.log("Receive peer 1 ice candidate", ice_candidate);
+      console.log("Receive caller ice candidate", ice_candidate);
       const candidate = new RTCIceCandidate(ice_candidate);
       pc.addIceCandidate(candidate);
     });
--- lib/simple_wrtc_demo_web/channels/call_channel.ex
+++ lib/simple_wrtc_demo_web/channels/call_channel.ex
@@ -15,32 +15,43 @@ defmodule SimpleWrtcDemoWeb.CallChannel do
 
   def join("call:" <> id, %{"type" => "callee"}, socket) do
     Logger.info("Callee Join call #{id}")
+
+    # When callee joins forward any existing ice candidates the caller might have send
     {:ok, CallsStore.get_candidates(id), assign(socket, :id, id)}
   end
 
+  # Used by callee to fetch the offer made by the caller
   def handle_in("get_offer", _args, socket) do
     {:reply, {:ok, CallsStore.get_offer(socket.assigns.id)}, socket}
   end
 
+  # Used by caller to create an offer and store it in the signaling server
+  # This offer will be later fetched by the callee
   def handle_in("offer", %{"offer" => offer}, socket) do
     CallsStore.create_offer(socket.assigns.id, offer)
 
     {:noreply, socket}
   end
 
+  # Used by callee to answer to a offer made by the caller
   def handle_in("answer", %{"answer" => answer}, socket) do
-    broadcast_from(socket, "new_answer", %{answer: answer})
+    broadcast_from(socket, "answer", %{answer: answer})
 
     {:noreply, socket}
   end
 
+  # Used by callee to send ice candidates, since the caller must be already connected to the channel
+  # before the callee we don't need to store the candidates and can directly broadcast them to the caller
   def handle_in("ice_candidate", %{"ice_candidate" => ice_candidate, "type" => "callee"}, socket) do
     broadcast_from(socket, "new_ice_candidate", %{ice_candidate: ice_candidate})
     {:noreply, socket}
   end
 
+  # Used by caller to send ice candidates, since the callee might not be present in the channel so
+  # we store the candidates which can be later fetched by the callee
   def handle_in("ice_candidate", %{"ice_candidate" => ice_candidate, "type" => "caller"}, socket) do
     CallsStore.save_candidate(socket.assigns.id, ice_candidate, "caller")
+    broadcast_from(socket, "new_ice_candidate", %{ice_candidate: ice_candidate})
     {:noreply, socket}
   end
 end
